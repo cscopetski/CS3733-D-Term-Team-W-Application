70b2163ce8070aab9877ed5484011b75b660babe
Jason Odell <jasonodell2001@gmail.com>
2022-04-01 19:33:17 -0400

    Began Setup

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/DefaultPage.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/DefaultPage.java
@@ -1,3 +1,11 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
-public class DefaultPage {}
+import edu.wpi.cs3733.d22.teamW.wMid.SceneManager;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+
+public class DefaultPage {
+  public void goMap(ActionEvent actionEvent) throws IOException {
+    SceneManager.getInstance().setScene(SceneManager.Scenes.MapEditor);
+  }
+}
--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/MapEditorController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/MapEditorController.java
@@ -1,3 +1,44 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
-public class MapEditorController {}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
+
+import javax.security.sasl.SaslServer;
+import javax.swing.text.html.ImageView;
+
+public class MapEditorController {
+  @FXML MenuItem F1;
+  @FXML MenuItem F2;
+  @FXML MenuItem F3;
+  @FXML MenuItem FL1;
+  @FXML MenuItem FL2;
+  @FXML ImageView map;
+
+  public void addLocation(MouseEvent mouseEvent) {}
+
+  public void refresh(ActionEvent actionEvent) {}
+
+  public void swapFloor1(ActionEvent actionEvent) {
+    System.out.println(F1.getText());
+
+  }
+
+  public void swapFloor2(ActionEvent actionEvent) {
+    System.out.println(F2.getText());
+  }
+
+  public void swapFloor3(ActionEvent actionEvent) {
+    System.out.println(F3.getText());
+  }
+
+  public void swapFloorL1(ActionEvent actionEvent) {
+    System.out.println(FL1.getText());
+  }
+
+  public void swapFloorL2(ActionEvent actionEvent) {
+    System.out.println(FL2.getText());
+  }
+}
