3a6239325fea77d43144c25325c09945d27e7032
phbui <101759075+phbui@users.noreply.github.com>
2022-04-01 20:11:39 -0400

    Window Working

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/DefaultPage.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/DefaultPage.java
@@ -1,3 +1,87 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
-public class DefaultPage {}
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+public class DefaultPage {
+
+  @FXML private Pane content;
+
+  public void switchToMedicineDelivery(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass()
+                    .getResource(
+                        "/edu/wpi/cs3733/d22/teamW/wApp/views/ServiceRequestPages/MedicineDeliveryServiceRequestPage.fxml")));
+  }
+
+  public void switchToLab(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass()
+                    .getResource(
+                        "/edu/wpi/cs3733/d22/teamW/wApp/views/ServiceRequestPages/LabServiceRequestPage.fxml")));
+  }
+
+  public void switchToMedicalEquipmentDelivery(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass()
+                    .getResource(
+                        "edu/wpi/cs3733/d22/teamW/wApp/views/ServiceRequestPages/MedicalEquipmentServiceRequestPage.fxml")));
+  }
+
+  public void switchToMealDelivery(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass()
+                    .getResource(
+                        "/edu/wpi/cs3733/d22/teamW/wApp/views/ServiceRequestPages/MealDeliveryServiceRequestPage.fxml")));
+  }
+
+  public void switchToLanguageInterpreter(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass()
+                    .getResource(
+                        "/edu/wpi/cs3733/d22/teamW/wApp/views/ServiceRequestPages/LanguageInterpreterServiceRequestPage.fxml")));
+  }
+
+  public void switchToSecurity(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass()
+                    .getResource(
+                        "/edu/wpi/cs3733/d22/teamW/wApp/views/ServiceRequestPages/SecurityServiceRequestPage.fxml")));
+  }
+
+  public void switchToMapEditor(ActionEvent event) throws IOException {
+    content.getChildren().clear();
+    content
+        .getChildren()
+        .add(
+            FXMLLoader.load(
+                getClass().getResource("/edu/wpi/cs3733/d22/teamW/wApp/views/MapEditorPage.fxml")));
+  }
+}
