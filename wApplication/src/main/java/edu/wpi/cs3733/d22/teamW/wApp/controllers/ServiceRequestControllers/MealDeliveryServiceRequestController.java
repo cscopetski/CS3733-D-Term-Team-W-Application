31fffb6f136f672c158e8fe751dd8ab5a7bd4e73
phbui <101759075+phbui@users.noreply.github.com>
2022-04-03 19:48:23 -0400

    Added Cleaner Buttons

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MealDeliveryServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MealDeliveryServiceRequestController.java
@@ -2,31 +2,50 @@ package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class MealDeliveryServiceRequestController {
-  @FXML VBox menuVBox1;
-  @FXML VBox menuVBox2;
-  @FXML VBox menuVBox3;
-  @FXML CheckBox option1;
-  @FXML CheckBox option2;
-  @FXML CheckBox option3;
+  @FXML Button menuButton1;
+  @FXML Button menuButton2;
+  @FXML Button menuButton3;
+  @FXML Label menuLabel1;
+  @FXML Label menuLabel2;
+  @FXML Label menuLabel3;
 
-  public void menuSelection1(ActionEvent actionEvent) {
-    option2.setSelected(false);
-    option3.setSelected(false);
+  private int currentSelection = 0;
+
+  public void menuSelection1() {
+    currentSelection = 1;
+    menuButton1.setStyle("-fx-background-color: #013895;");
+    menuButton2.setStyle("-fx-background-color: transparent;");
+    menuButton3.setStyle("-fx-background-color: transparent;");
+    menuLabel1.setStyle("-fx-text-fill: white");
+    menuLabel2.setStyle("-fx-text-fill: black");
+    menuLabel3.setStyle("-fx-text-fill: black");
   }
 
-  public void menuSelection2(ActionEvent actionEvent) {
-    option1.setSelected(false);
-    option3.setSelected(false);
+  public void menuSelection2() {
+    currentSelection = 2;
+    menuButton1.setStyle("-fx-background-color: transparent");
+    menuButton2.setStyle("-fx-background-color: #013895;");
+    menuButton3.setStyle("-fx-background-color: transparent;");
+    menuLabel1.setStyle("-fx-text-fill: black");
+    menuLabel2.setStyle("-fx-text-fill: white");
+    menuLabel3.setStyle("-fx-text-fill: black");
   }
 
-  public void menuSelection3(ActionEvent actionEvent) {
-    option1.setSelected(false);
-    option2.setSelected(false);
+  public void menuSelection3() {
+    currentSelection = 3;
+    menuButton1.setStyle("-fx-background-color: transparent");
+    menuButton2.setStyle("-fx-background-color: transparent;");
+    menuButton3.setStyle("-fx-background-color: #013895;");
+    menuLabel1.setStyle("-fx-text-fill: black");
+    menuLabel2.setStyle("-fx-text-fill: black");
+    menuLabel3.setStyle("-fx-text-fill: white");
   }
 
-  public void submitButton(ActionEvent actionEvent) {}
+  public void submitButton(ActionEvent actionEvent) {
+    if (currentSelection != 0) {}
+  }
 }
