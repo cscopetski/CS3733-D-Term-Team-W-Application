62bc450bb7e9df50af301192d363ee4eea599059
phbui <101759075+phbui@users.noreply.github.com>
2022-04-03 18:24:10 -0400

    Made 1 CheckBox checked maximum

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MealDeliveryServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MealDeliveryServiceRequestController.java
@@ -1,3 +1,34 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
-public class MealDeliveryServiceRequestController {}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+
+public class MealDeliveryServiceRequestController {
+  @FXML VBox menuVBox1;
+  @FXML VBox menuVBox2;
+  @FXML VBox menuVBox3;
+  @FXML CheckBox option1;
+  @FXML CheckBox option2;
+  @FXML CheckBox option3;
+
+  public void menuSelection1(ActionEvent actionEvent) {
+    option2.setSelected(false);
+    option3.setSelected(false);
+  }
+
+  public void menuSelection2(ActionEvent actionEvent) {
+    option1.setSelected(false);
+    option3.setSelected(false);
+  }
+
+  public void menuSelection3(ActionEvent actionEvent) {
+    option1.setSelected(false);
+    option2.setSelected(false);
+  }
+
+  public void submitButton(ActionEvent actionEvent) {
+
+  }
+}
