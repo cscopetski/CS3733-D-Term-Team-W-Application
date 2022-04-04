21dc5544a0457e2f250e83ecb7177a59eb093061
phbui <101759075+phbui@users.noreply.github.com>
2022-04-03 21:16:34 -0400

    Added UI

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicalEquipmentServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicalEquipmentServiceRequestController.java
@@ -1,3 +1,32 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
-public class MedicalEquipmentServiceRequestController {}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+
+public class MedicalEquipmentServiceRequestController {
+  Alert confirm =
+      new Alert(
+          Alert.AlertType.CONFIRMATION,
+          "Would you like to confirm this request " + " ?",
+          ButtonType.OK,
+          ButtonType.CANCEL);
+  Alert emptyFields =
+      new Alert(
+          Alert.AlertType.ERROR,
+          "There are required fields empty " + " !",
+          ButtonType.OK,
+          ButtonType.CANCEL);
+  @FXML ComboBox<String> equipmentSelection;
+
+  public void submitButton(ActionEvent actionEvent) {
+    if (!equipmentSelection.getValue().isEmpty()) {
+      confirm.show();
+      if (confirm.getResult() == ButtonType.OK) {}
+    } else {
+      emptyFields.show();
+    }
+  }
+}
