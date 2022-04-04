171cf04e17e6d927ee32430d784a3effb1ae00ff
Ethan Pollack <epollack31@gmail.com>
2022-04-03 20:38:07 -0400

    Found Error: You are trying to reference TextField's in the controller but they do not exist in FXML. This was causing an issue in the DefaultPage even though it isn't actually happening in there because I am loading all pages from DefaultPage at the beginning so it was trying to load that page and failing

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
@@ -1,25 +1,22 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
 import java.awt.*;
-import javafx.fxml.FXML;
 
 public class MedicineDeliveryServiceRequestController {
 
   // FXML connectors:
-  @FXML TextField location;
-  @FXML TextField employee;
-  @FXML TextField medicine;
+  // @FXML TextField location;
+  // @FXML TextField employee;
+  // @FXML TextField medicine;
 
-  public void clearFields() {
+  public void clearFields() {}
 
-  }
-
-  public void submitButton(){
+  public void submitButton() {
     clearFields();
     createRequest();
   }
 
-  //NEEDS LINK TO DB
+  // NEEDS LINK TO DB
   public void createRequest() {
     //
   }
