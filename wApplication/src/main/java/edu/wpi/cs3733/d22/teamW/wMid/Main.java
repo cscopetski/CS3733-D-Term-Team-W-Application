c662a552b08b8f3824c2a91ca6e31acf0ec3d28a
cscopetski <76232197+cscopetski@users.noreply.github.com>
2022-04-04 17:51:15 -0400

    Made LabServiceRequests Table

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wMid/Main.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wMid/Main.java
@@ -1,86 +1,85 @@
 package edu.wpi.cs3733.d22.teamW.wMid;
 
 import edu.wpi.cs3733.d22.teamW.wDB.*;
-
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, FileNotFoundException {
+  public static void main(String[] args) throws SQLException, FileNotFoundException {
 
-        // App.launch(App.class, args);
+    // App.launch(App.class, args);
 
-        final String locationFileName = "TowerLocations.csv";
-        final String medEquipFileName = "MedicalEquipment.csv";
-        final String medEquipRequestFileName = "MedicalEquipmentRequest.csv";
+    final String locationFileName = "TowerLocations.csv";
+    final String medEquipFileName = "MedicalEquipment.csv";
+    final String medEquipRequestFileName = "MedicalEquipmentRequest.csv";
 
-        DBController.getDBController();
+    DBController.getDBController();
 
-        CSVController csvController =
-                new CSVController(locationFileName, medEquipFileName, medEquipRequestFileName);
+    CSVController csvController =
+        new CSVController(locationFileName, medEquipFileName, medEquipRequestFileName);
 
-        try {
-            csvController.populateEntityTables();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        MedEquipDaoImpl medi = new MedEquipDaoImpl();
-        MedEquipRequestDaoImpl merdi = new MedEquipRequestDaoImpl();
-        MedEquipRequestController merc = new MedEquipRequestController(merdi, medi);
-
-        RequestFactory requestFactory = RequestFactory.getRequestFactory(merc);
-
-        csvController.populateRequestTables(requestFactory);
-
-        // itemType, location(nodeID), employeeName, (String) isEmergency (0=no, 1=yes)
-
-        ArrayList<String> fields = new ArrayList<>();
-
-        fields.add("XRY");
-        fields.add("wSTOR001L1");
-        fields.add("JOE NAME");
-        fields.add("" + 0);
-
-        ArrayList<String> fields2 = new ArrayList<>();
-
-        fields2.add("XRY");
-        fields2.add("wSTOR001L1");
-        fields2.add("JOE2 NAME");
-        fields2.add("" + 1);
-
-        ArrayList<String> fields3 = new ArrayList<>();
-
-        fields3.add("XRY");
-        fields3.add("wSTOR001L1");
-        fields3.add("JOE2 NAME");
-        fields3.add("" + 0);
-
-        try {
-            requestFactory.getRequest("MEDEQUIPREQUEST", fields);
-            requestFactory.getRequest("MEDEQUIPREQUEST", fields2);
-            requestFactory.getRequest("MEDEQUIPREQUEST", fields3);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        Request test = requestFactory.findRequest(5);
-        Request test2 = requestFactory.findRequest(11);
-        Request test3 = requestFactory.findRequest(12);
-        Request test4 = requestFactory.findRequest(13);
-        // completes test
-        merc.completeRequest(test);
-        // Tries to cancel test but fails since it is completed
-        merc.cancelRequest(test);
-        // test 2 should be enqueue then cancelled starting test 3
-        merc.cancelRequest(test2);
-        merc.completeRequest(test2);
-        merc.cancelRequest(test3);
-        merc.completeRequest(test3);
+    try {
+      csvController.populateEntityTables();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+
+    MedEquipDaoImpl medi = new MedEquipDaoImpl();
+    MedEquipRequestDaoImpl merdi = new MedEquipRequestDaoImpl();
+    MedEquipRequestController merc = new MedEquipRequestController(merdi, medi);
+
+    RequestFactory requestFactory = RequestFactory.getRequestFactory(merc);
+
+    csvController.populateRequestTables(requestFactory);
+
+    // itemType, location(nodeID), employeeName, (String) isEmergency (0=no, 1=yes)
+
+    ArrayList<String> fields = new ArrayList<>();
+
+    fields.add("XRY");
+    fields.add("wSTOR001L1");
+    fields.add("JOE NAME");
+    fields.add("" + 0);
+
+    ArrayList<String> fields2 = new ArrayList<>();
+
+    fields2.add("XRY");
+    fields2.add("wSTOR001L1");
+    fields2.add("JOE2 NAME");
+    fields2.add("" + 1);
+
+    ArrayList<String> fields3 = new ArrayList<>();
+
+    fields3.add("XRY");
+    fields3.add("wSTOR001L1");
+    fields3.add("JOE2 NAME");
+    fields3.add("" + 0);
+
+    try {
+      requestFactory.getRequest("MEDEQUIPREQUEST", fields);
+      requestFactory.getRequest("MEDEQUIPREQUEST", fields2);
+      requestFactory.getRequest("MEDEQUIPREQUEST", fields3);
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    Request test = requestFactory.findRequest(5);
+    Request test2 = requestFactory.findRequest(11);
+    Request test3 = requestFactory.findRequest(12);
+    Request test4 = requestFactory.findRequest(13);
+    // completes test
+    merc.completeRequest(test);
+    // Tries to cancel test but fails since it is completed
+    merc.cancelRequest(test);
+    // test 2 should be enqueue then cancelled starting test 3
+    merc.cancelRequest(test2);
+    merc.completeRequest(test2);
+    merc.cancelRequest(test3);
+    merc.completeRequest(test3);
+  }
 }
