66cc1e60db9074efa34ce36cd8e44e5657f16dc6
cscopetski <cscopetski@gmail.com>
2022-04-03 21:42:52 -0400

    Added gitignore

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicalEquipmentServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicalEquipmentServiceRequestController.java
@@ -1,9 +1,3 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
-import edu.wpi.cs3733.d22.teamW.wDB.Request;
-import edu.wpi.cs3733.d22.teamW.wDB.RequestController;
-import edu.wpi.cs3733.d22.teamW.wDB.RequestFactory;
-
-public class MedicalEquipmentServiceRequestController {
-
-}
+public class MedicalEquipmentServiceRequestController {}
