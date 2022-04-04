19c6670a1a7dc96d47a5aea9ef4d18296feae801
Ethan Pollack <epollack31@gmail.com>
2022-04-04 17:23:27 -0400

    Added Main Menu button to Menu Bar

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/RequestListPageController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/RequestListPageController.java
@@ -3,26 +3,20 @@ package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 import edu.wpi.cs3733.d22.teamW.wApp.controllers.LoadableController;
 import edu.wpi.cs3733.d22.teamW.wMid.SceneManager;
 import javafx.fxml.FXML;
-
 import javax.swing.table.TableColumn;
 
 public class RequestListPageController extends LoadableController {
-    @FXML TableColumn reqID;
-    @FXML TableColumn reqType;
-    @FXML TableColumn employeeName;
-    @FXML TableColumn numQueue;
+  @FXML TableColumn reqID;
+  @FXML TableColumn reqType;
+  @FXML TableColumn employeeName;
+  @FXML TableColumn numQueue;
 
-    @Override
-    protected SceneManager.Scenes GetSceneType() {
-        return SceneManager.Scenes.RequestList;
-    }
+  @Override
+  protected SceneManager.Scenes GetSceneType() {
+    return SceneManager.Scenes.RequestList;
+  }
 
-    public void onLoad() {
+  public void onLoad() {}
 
-    }
-
-    public void onUnload() {
-
-    }
-
+  public void onUnload() {}
 }
