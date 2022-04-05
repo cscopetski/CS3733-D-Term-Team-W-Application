55eb58fce42c1ff81957c5b5462765c6b6004fbc
EdwardEnyedy <emenyedy@wpi.edu>
2022-04-05 12:14:26 -0400

    more work on visuals of page

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
@@ -1,6 +1,10 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
 import java.awt.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 public class MedicineDeliveryServiceRequestController {
 
@@ -9,15 +13,39 @@ public class MedicineDeliveryServiceRequestController {
   // @FXML TextField employee;
   // @FXML TextField medicine;
 
-  public void clearFields() {}
+  // Combo Boxes:
+  @FXML ComboBox medNameCBox;
+  @FXML ComboBox freeCBox;
+  @FXML ComboBox locationCBox;
+  @FXML ComboBox timePrefCBox;
+
+  // PULL FROM DB OF MEDS??
+  ObservableList<String> meds = FXCollections.observableArrayList("Advil", "Sleep");
+
+  public void clearFields() {
+    //
+  }
+
+  public void temp() {
+    medNameCBox.setItems(meds);
+  }
+
+  public void createRequest() {
+    // NEEDS LINK TO DB
+  }
 
   public void submitButton() {
     clearFields();
     createRequest();
   }
 
-  // NEEDS LINK TO DB
-  public void createRequest() {
-    //
+  // getters and setters for combo box's list
+  // -unsure if needed
+  public ObservableList<String> getMeds() {
+    return meds;
+  }
+
+  public void setMeds(ObservableList<String> meds) {
+    this.meds = meds;
   }
 }
