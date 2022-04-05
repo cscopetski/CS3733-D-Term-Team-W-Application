fcd75cf96272b7a1d86af30e564faf6f31fc6e0a
EdwardEnyedy <emenyedy@wpi.edu>
2022-04-05 14:28:01 -0400

    more tweeks on graphics. Need help on adding to table

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
@@ -7,50 +7,66 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class MedicineDeliveryServiceRequestController extends LoadableController {
 
-  // Textfields:
+  // TextFields:
   @FXML TextField quantityField;
   @FXML TextField itemCodeField;
 
   // Buttons:
   @FXML Button addButton;
   @FXML Button submitButton;
+  @FXML Button cancelButton;
 
-  // Combo Boxes:
+  // ComboBoxes:
   @FXML ComboBox medNameCBox;
   @FXML ComboBox locationCBox;
   @FXML ComboBox timePrefCBox;
   @FXML ComboBox requesterCBox; // FREE BOX, NOT SURE WHAT TO DO
 
-  // PULL FROM DB //
-  ObservableList<String> meds = FXCollections.observableArrayList("Advil", "Sleep");
-  ObservableList<String> locations = FXCollections.observableArrayList("Advil", "Sleep");
+  // ComboBox Lists:
+  ObservableList<String> meds = FXCollections.observableArrayList("create list in DB");
+  ObservableList<String> locations = FXCollections.observableArrayList("get from DB");
   ObservableList<String> times =
-      FXCollections.observableArrayList("8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30");
+      FXCollections.observableArrayList(
+          "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00",
+          "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00",
+          "18:30");
 
-  public void clearFields() {
-    //
-  }
+  // Tables:
+  @FXML TableView orderTable;
 
-  @Override
+  // Table Lists:
+  ObservableList<String> orders = FXCollections.observableArrayList();
+
   protected SceneManager.Scenes GetSceneType() {
     return SceneManager.Scenes.MedicineDelivery;
+  }
+
+  // clear all UI inputs:
+  public void clearFields() {
+    quantityField.clear();
+    itemCodeField.clear();
+    medNameCBox.setValue(null);
+    locationCBox.setValue(null);
+    timePrefCBox.setValue(null);
+    requesterCBox.setValue(null);
   }
 
   public void onLoad() {
     medNameCBox.setItems(meds);
     locationCBox.setItems(locations);
     timePrefCBox.setItems(times);
+    orderTable.setItems(orders);
   }
 
-  @Override
   public void onUnload() {}
 
   public void createRequest() {
-    // NEEDS LINK TO DB
+    orders.add(0, "hi");
   }
 
   public void submitButton() {
