5eeb064c941a8ebdaadbd6f2215c1fdf5b1a9964
EdwardEnyedy <emenyedy@wpi.edu>
2022-04-05 13:01:12 -0400

    more scenebuilder work

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/MedicineDeliveryServiceRequestController.java
@@ -1,33 +1,43 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
-import java.awt.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class MedicineDeliveryServiceRequestController {
 
-  // FXML connectors:
-  // @FXML TextField location;
-  // @FXML TextField employee;
-  // @FXML TextField medicine;
+  // Textfields:
+  @FXML TextField quantityField;
+  @FXML TextField itemCodeField;
+
+  // Buttons:
+  @FXML Button addButton;
+  @FXML Button submitButton;
+
 
   // Combo Boxes:
   @FXML ComboBox medNameCBox;
-  @FXML ComboBox freeCBox;
   @FXML ComboBox locationCBox;
   @FXML ComboBox timePrefCBox;
+  @FXML ComboBox requesterCBox; // FREE BOX, NOT SURE WHAT TO DO
 
-  // PULL FROM DB OF MEDS??
+  // PULL FROM DB //
   ObservableList<String> meds = FXCollections.observableArrayList("Advil", "Sleep");
+  ObservableList<String> locations = FXCollections.observableArrayList("Advil", "Sleep");
+  ObservableList<String> times =
+      FXCollections.observableArrayList("8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30");
 
   public void clearFields() {
     //
   }
 
-  public void temp() {
+  public void tempStartup() {
     medNameCBox.setItems(meds);
+    locationCBox.setItems(locations);
+    timePrefCBox.setItems(times);
   }
 
   public void createRequest() {
