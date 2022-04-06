94f18d31eda4220bb177d292892d41dd2373dc97
cscopetski <76232197+cscopetski@users.noreply.github.com>
2022-04-05 22:53:30 -0400

    Added DBControllerTest method

--- wApplication/src/test/java/edu/wpi/cs3733/d22/teamW/wDB/MedEquipDaoImplTest.java
+++ wApplication/src/test/java/edu/wpi/cs3733/d22/teamW/wDB/MedEquipDaoImplTest.java
@@ -1,33 +1,26 @@
 package edu.wpi.cs3733.d22.teamW.wDB;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class MedEquipDaoImplTest {
 
-    @Test
-    void setMedEquipList() {
-    }
+  @Test
+  void setMedEquipList() {}
 
-    @Test
-    void getAllMedEquip() {
-    }
+  @Test
+  void getAllMedEquip() {}
 
-    @Test
-    void addMedEquip() {
-    }
+  @Test
+  void addMedEquip() {}
 
-    @Test
-    void deleteMedEquip() {
-    }
+  @Test
+  void deleteMedEquip() {}
 
-    @Test
-    void changeMedEquip() {
-    }
+  @Test
+  void changeMedEquip() {}
 
-    @Test
-    void setStatus() {
-    }
-
+  @Test
+  void setStatus() {}
 }
