c46209519613c0bfb6e5204a1af48031e6bc2d29
FlaggedID <ezhang2@wpi.edu>
2022-04-05 23:52:19 -0400

    MedEquipTesting still failed but yeah

--- wApplication/src/test/java/edu/wpi/cs3733/d22/teamW/wDB/MedEquipRequestControllerTest.java
+++ wApplication/src/test/java/edu/wpi/cs3733/d22/teamW/wDB/MedEquipRequestControllerTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
-import org.junit.jupiter.api.BeforeAll;
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MedEquipRequestControllerTest {
 
-  DBController dbController;
+  DBController dbController = DBController.getDBController();
   CSVController csvController;
   LocationDaoImpl locationDao;
   LocationController locationController;
@@ -25,14 +25,12 @@ class MedEquipRequestControllerTest {
 
   RequestFactory requestFactory;
 
-  @BeforeAll
-  void setUp() throws SQLException, FileNotFoundException {
+  @BeforeEach
+  void setUp() {
     final String locationFileName = "TowerLocations.csv";
     final String medEquipFileName = "MedicalEquipment.csv";
     final String medEquipRequestFileName = "MedicalEquipmentRequest.csv";
     final String labServiceRequestFileName = "LabRequests.csv";
-
-    dbController = DBController.getDBController();
 
     csvController =
         new CSVController(
@@ -45,14 +43,18 @@ class MedEquipRequestControllerTest {
     } catch (SQLException e) {
       e.printStackTrace();
     }
-  }
-
-  @BeforeEach
-  void setup2() throws SQLException, FileNotFoundException {
-    locationDao = new LocationDaoImpl();
+    try {
+      locationDao = new LocationDaoImpl();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
     locationController = new LocationController(locationDao);
 
-    medi = new MedEquipDaoImpl();
+    try {
+      medi = new MedEquipDaoImpl();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
     merdi = new MedEquipRequestDaoImpl();
     medEquipController = new MedEquipController(medi, merdi);
     merc = new MedEquipRequestController(merdi, medi);
@@ -62,33 +64,74 @@ class MedEquipRequestControllerTest {
 
     requestFactory = RequestFactory.getRequestFactory(merc, lsrc);
 
-    csvController.populateRequestTables(requestFactory);
+    try {
+      csvController.populateRequestTables(requestFactory);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   @Test
-  void checkStart() {
-    Request request = RequestFactory.getRequestFactory().findRequest(6);
-    String test = merc.checkStart();
+  void checkStart() throws SQLException {
+    Request request = RequestFactory.getRequestFactory().findRequest(5);
+    String test = merc.checkStart(request);
+    assertNull(test);
   }
 
   @Test
-  void cancelRequest() {}
+  void cancelRequest() throws SQLException {
+    Request request = RequestFactory.getRequestFactory().findRequest(5);
+    merc.cancelRequest(request);
+    assertEquals(request.getStatus(), 3);
+  }
 
   @Test
-  void completeRequest() {}
+  void completeRequest() throws SQLException {
+    Request request = RequestFactory.getRequestFactory().findRequest(8);
+    merc.completeRequest(request);
+    assertEquals(request.getStatus(), 2);
+  }
 
   @Test
-  void checkNext() {}
+  void checkNext() throws SQLException {
+    merc.checkNext("BED001");
+  }
 
   @Test
-  void getNext() {}
+  void getNext() {
+    Request request = merc.getNext("BED001");
+    assertEquals(2, request.getRequestID());
+  }
 
   @Test
-  void getRequest() {}
+  void getRequest() {
+    Request request = merc.getRequest(2);
+    assertEquals(request.getRequestID(), 2);
+  }
 
   @Test
-  void addRequest() {}
+  void addRequest() throws SQLException {
+    ArrayList<String> fields3 = new ArrayList<>();
+
+    fields3.add("XRY");
+    fields3.add("wSTOR001L1");
+    fields3.add("JOE2 NAME");
+    fields3.add("" + 0);
+    Request request = merc.addRequest(11, fields3);
+    assertEquals(request.getRequestID(), 11);
+  }
 
   @Test
-  void getAllMedEquipRequests() {}
+  void getAllMedEquipRequests() {
+    ArrayList<MedEquipRequest> requests = merc.getAllMedEquipRequests();
+    Request request = null;
+    for (Request r : requests) {
+      if (r.getRequestID() == 2) {
+        request = r;
+      }
+    }
+    assertEquals(request.getRequestID(), 2);
+  }
 }
