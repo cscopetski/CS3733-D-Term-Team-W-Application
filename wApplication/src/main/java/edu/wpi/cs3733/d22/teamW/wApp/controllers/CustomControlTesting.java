a346af0d870016a07298bd51cfb381cb412b987e
Ethan Pollack <epollack31@gmail.com>
2022-04-07 19:26:07 -0400

    Still Not Working

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/CustomControlTesting.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/CustomControlTesting.java
@@ -1,19 +1,16 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
-import edu.wpi.cs3733.d22.teamW.wApp.controllers.CustomControls.EmergencyButton;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
 
 public class CustomControlTesting implements Initializable {
-  @FXML public AnchorPane ap;
-  @FXML public EmergencyButton eb;
+  // @FXML public AnchorPane ap;
+  // @FXML public EmergencyButton eb;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    ap.getChildren().add(new EmergencyButton());
+    // ap.getChildren().add(new EmergencyButton());
   }
   // @FXML public EmergencyButton eb;
 
