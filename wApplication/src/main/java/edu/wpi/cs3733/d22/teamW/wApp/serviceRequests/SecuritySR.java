f430e3cf50b28ad43722e2d71bfb235f56524334
NhiNguyencmt8 <61450663+NhiNguyencmt8@users.noreply.github.com>
2022-04-08 15:47:15 -0400

    Fixed emergency buttons

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/serviceRequests/SecuritySR.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/serviceRequests/SecuritySR.java
@@ -2,14 +2,14 @@ package edu.wpi.cs3733.d22.teamW.wApp.serviceRequests;
 
 import edu.wpi.cs3733.d22.teamW.wDB.Request;
 
-public class SecuritySR extends SR{
+public class SecuritySR extends SR {
 
-    public SecuritySR(Request r) {
-        super(r);
-    }
+  public SecuritySR(Request r) {
+    super(r);
+  }
 
-    @Override
-    public String getRequestType() {
-        return "Security Service";
-    }
+  @Override
+  public String getRequestType() {
+    return "Security Service";
+  }
 }
