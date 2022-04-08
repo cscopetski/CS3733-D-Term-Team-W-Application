0339191b860ed652587464db94f78cf936c96182
Ethan Pollack <epollack31@gmail.com>
2022-04-08 11:32:01 -0400

    Made Custom RequestTable

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/CustomControlTesting.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/CustomControlTesting.java
@@ -1,11 +1,13 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
+import edu.wpi.cs3733.d22.teamW.wApp.controllers.customControls.RequestTable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class CustomControlTesting implements Initializable {
-  // @FXML private TableView<SR> table;
+  @FXML public RequestTable table;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/customControls/RequestTable.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/customControls/RequestTable.java
@@ -12,32 +12,58 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class RequestTable extends TableView<SR> {
 
-  public RequestTable() { // Class<SR> requestType
+  public RequestTable() {
     super();
-    TableColumn requestIDColumn = new TableColumn("Request ID");
-    TableColumn requestTypeColumn = new TableColumn("Request Type");
-    TableColumn employeeNameColumn = new TableColumn("Employee Name");
-    TableColumn statusColumn = new TableColumn("Status");
-    requestIDColumn.setCellValueFactory(new PropertyValueFactory("RequestID"));
-    requestTypeColumn.setCellValueFactory(new PropertyValueFactory("RequestType"));
-    employeeNameColumn.setCellValueFactory(new PropertyValueFactory("EmployeeName"));
-    statusColumn.setCellValueFactory(new PropertyValueFactory("Status"));
-    // figure uot how to add the property value factory to the column
-    getColumns().addAll(requestIDColumn, requestTypeColumn, employeeNameColumn, statusColumn);
+    getStylesheets().add("edu/wpi/cs3733/d22/teamW/wApp/CSS/UniversalCSS/Standard.css");
 
-    /*List<SR> requests =
-            RequestFactory.getRequestFactory().getAllRequests().stream()
-                .map(MedicalEquipmentSR::new)
-                .collect(Collectors.toList());
-    */
+    getColumns()
+        .addAll(
+            createColumn("Request ID", "RequestID"),
+            createColumn("Request Type", "RequestType"),
+            createColumn("Employee Name", "EmployeeName"),
+            createColumn("Status", "Status"));
+
+    // distributeColumnWidths();
     List<Request> requests = RequestFactory.getRequestFactory().getAllRequests();
-    for (Request r : requests) {
-      if (r.getRequestType().equals("MEDICALEQUIPREQUEST")) {
-        this.getItems().add(new MedicalEquipmentSR(r));
-      } else if (r.getRequestType().equals("LABSERVICEREQUEST")) {
-        this.getItems().add(new LabServiceSR(r));
+    setItems(requests);
+  }
+
+  public void setColumnWidth(String columnText, double prefWidth) {
+    for (TableColumn c : getColumns()) {
+      if (c.getText().equals(columnText)) {
+        c.setPrefWidth(prefWidth);
+        return;
       }
     }
-    // this.getItems().addAll(requests);
+  }
+
+  private TableColumn createColumn(String columnText, String propertyName) {
+    TableColumn column = new TableColumn(columnText);
+    column.setCellValueFactory(new PropertyValueFactory(propertyName));
+    return column;
+  }
+
+  public void distributeColumnWidths() {
+    for (TableColumn c : getColumns()) {
+      c.prefWidthProperty().bind(widthProperty().divide(4));
+    }
+  }
+
+  private void setItems(List<Request> requests) {
+    getItems().clear();
+    for (Request r : requests) {
+      SR sr = null;
+      if (r.getRequestType().equals("MEDICALEQUIPREQUEST")) {
+        sr = new MedicalEquipmentSR(r);
+      } else if (r.getRequestType().equals("LABSERVICEREQUEST")) {
+        sr = new LabServiceSR(r);
+      }
+      getItems().add(sr);
+    }
+    getSelectionModel().clearSelection();
+  }
+
+  public SR getSelection() {
+    return getSelectionModel().getSelectedItem();
   }
 }
