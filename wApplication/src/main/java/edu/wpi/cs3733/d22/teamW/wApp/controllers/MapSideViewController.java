cf9814ce58d6eabb12f10414720da36e278b414c
Ethan Pollack <epollack31@gmail.com>
2022-04-10 18:37:19 -0400

    Panning/Zooming working

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/MapSideViewController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/MapSideViewController.java
@@ -4,26 +4,24 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 
 public class MapSideViewController {
-  public ScrollPane scrollPane;
+  @FXML public ScrollPane scrollPane;
   @FXML public ImageView towerMap;
-  public Pane towerPane;
   @FXML private Slider slider;
 
   public void initialize() {
-    slider.setValue(1);
-    slider.setMin(1);
-    slider.setMax(5);
-    //scaling the pane to make it fit with the slider value
-    towerPane.scaleXProperty().bind(slider.valueProperty());
-    towerPane.scaleYProperty().bind(slider.valueProperty());
-    scrollPane.setHvalue(towerMap.getScaleX());
-    scrollPane.setVvalue(towerMap.getScaleY());
+    slider.setValue(2.0);
+    slider.setMin(1.0);
+    slider.setMax(3.0);
+
+    towerMap.scaleXProperty().bind(slider.valueProperty());
+    towerMap.scaleYProperty().bind(slider.valueProperty());
+    towerMap.autosize();
     scrollPane.setPannable(true);
     scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
     scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-
+    // scrollPane.getContent().scaleXProperty().bind(slider.valueProperty());
+    // scrollPane.getContent().scaleYProperty().bind(slider.valueProperty());
   }
 }
