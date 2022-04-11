8992b7d952a9f6cacf8da246994a220135f52dd1
cscopetski <76232197+cscopetski@users.noreply.github.com>
2022-04-11 16:29:06 -0400

    Added import from file for CSVController

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wMid/Main.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wMid/Main.java
@@ -4,6 +4,7 @@ import edu.wpi.cs3733.d22.teamW.wDB.*;
 import edu.wpi.cs3733.d22.teamW.wDB.DAO.DBController;
 import edu.wpi.cs3733.d22.teamW.wDB.Managers.*;
 import edu.wpi.cs3733.d22.teamW.wDB.entity.Request;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -38,6 +39,16 @@ public class Main {
       e.printStackTrace();
     } catch (SQLException e) {
       e.printStackTrace();
+    }
+
+    File file =
+        new File(
+            "C:/Users/cscop/OneDrive/WPI/2021-2022/Classes/D-Term/CS3733/Minimal Application/WumboWhowies/wApplication/LABTEST.csv");
+
+    for (String[] s : csvController.importCSVfromFile(file)) {
+      for (String d : s) {
+        System.out.println(d);
+      }
     }
 
     MedEquipRequestManager merc = MedEquipRequestManager.getMedEquipRequestManager();
