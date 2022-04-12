ac8d18ea463674578c9212cac60bbcb942527304
phbui <101759075+phbui@users.noreply.github.com>
2022-04-12 00:17:19 -0400

    Color Correction

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/Snake/Food.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/Snake/Food.java
@@ -25,8 +25,7 @@ public class Food {
       pane.getChildren().remove(rectangle);
     }
     rectangle = new Rectangle(position.getXPos(), position.getYPos(), size, size);
-    Color color = new Color(0, 139, 176, 1.0);
-    rectangle.setFill(color);
+    rectangle.setFill(Color.rgb(0, 139, 176, 1.0));
     pane.getChildren().add(rectangle);
     System.out.println(rectangle);
   }
