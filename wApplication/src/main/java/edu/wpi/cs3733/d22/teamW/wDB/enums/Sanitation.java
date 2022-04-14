c7be200ef300f9007b58da3aa710a6f9f40d590c
Edison Zhang <ezhang2@wpi.edu>
2022-04-13 20:40:26 -0400

    Sanitation Enum for Sanitation Request

---
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wDB/enums/Sanitation.java
@@ -0,0 +1,38 @@
+package edu.wpi.cs3733.d22.teamW.wDB.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Sanitation {
+  RoomCleaning("Room Cleaning"),
+  Mopping("Mopping"),
+  PatientExcretion("Patient Execretion"),
+  Spill("Spills"),
+  ChemicalSpill("Chemical Spill");
+
+  public final String name;
+
+  private static Map map = new HashMap<>();
+
+  private Sanitation(String name) {
+    this.name = name;
+  }
+
+  static {
+    for (Sanitation type : Sanitation.values()) {
+      map.put(type.name, type);
+    }
+  }
+
+  public String getString() {
+    return this.name;
+  }
+
+  public Sanitation getSanitation(String type) throws Exception {
+    Sanitation sanType = (Sanitation) map.get(type);
+    if (sanType == null) {
+      throw (new Exception("Sanitation Request Choice does not exist in the enum"));
+    }
+    return sanType;
+  }
+}
