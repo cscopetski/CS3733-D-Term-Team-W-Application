c23121689d8af12ee9a1225f07408add97244dce
charkw <cckneisslwilliam@wpi.edu>
2022-04-15 22:16:33 -0400

    FlowerDao and Impl skeleton

---
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wDB/DAO/FlowerDao.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs3733.d22.teamW.wDB.DAO;
+
+import edu.wpi.cs3733.d22.teamW.wDB.entity.LabServiceRequest;
+import edu.wpi.cs3733.d22.teamW.wDB.entity.Request;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface FlowerDao {
+
+    ArrayList<Request> getFlowerRequests();
+
+    void addFlowerRequest(LabServiceRequest lsr) throws SQLException;
+
+    void changeFlowerRequest(LabServiceRequest lsr) throws SQLException;
+
+    void deleteFlowerRequest(Integer requestID) throws SQLException;
+
+    void exportFlowerReqCSV(String filename);
+
+    ArrayList<Request> getEmployeeRequests(Integer employeeID);
+}
