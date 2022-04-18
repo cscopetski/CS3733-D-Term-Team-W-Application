92b6e406a9a89e338de41f8730cde37c32b39086
Jack McEvoy <jackmcevoy15@gmail.com>
2022-04-18 18:46:08 -0400

    Basic Functionality (no DB connection)

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/SecurityServiceRequestController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/ServiceRequestControllers/SecurityServiceRequestController.java
@@ -1,22 +1,36 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers.ServiceRequestControllers;
 
 import edu.wpi.cs3733.d22.teamW.wApp.controllers.ConfirmAlert;
+import edu.wpi.cs3733.d22.teamW.wApp.controllers.EmptyAlert;
+import edu.wpi.cs3733.d22.teamW.wApp.controllers.LoadableController;
+import edu.wpi.cs3733.d22.teamW.wApp.controllers.customControls.AutoCompleteInput;
 import edu.wpi.cs3733.d22.teamW.wApp.controllers.customControls.EmergencyButton;
+import edu.wpi.cs3733.d22.teamW.wDB.Managers.LocationManager;
+import edu.wpi.cs3733.d22.teamW.wDB.entity.Location;
+import edu.wpi.cs3733.d22.teamW.wMid.SceneManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 
-public class SecurityServiceRequestController {
+public class SecurityServiceRequestController extends LoadableController {
 
   Alert confirm = new ConfirmAlert();
+  Alert emptyFields = new EmptyAlert();
 
-  public TextField roomNumber;
   // boolean emergencyLevel = false;
   int emergency = 0;
 
   @FXML EmergencyButton emergencyB;
+  @FXML ChoiceBox threatChoice;
+  @FXML AutoCompleteInput locationBox;
+
+  ArrayList<String> threatLevels = new ArrayList<>(Arrays.asList("Wong", "Red", "Yellow", "Blue"));
 
   //  public void emergencyClicked(MouseEvent mouseEvent) {
   //    if (emergencyLevel) {
@@ -39,11 +53,63 @@ public class SecurityServiceRequestController {
   //    }
   //  }
 
+  @Override
+  protected SceneManager.Scenes GetSceneType() {
+    return SceneManager.Scenes.Security;
+  }
+
+  @Override
+  public void onLoad() throws SQLException {
+    threatChoice.setItems(FXCollections.observableArrayList(threatLevels));
+    locationBox.loadValues(getLocations());
+  }
+
+  @Override
+  public void onUnload() {
+    clearFields();
+  }
+
   public void submitButton(ActionEvent actionEvent) throws SQLException {
-    confirm.showAndWait();
+    if (checkEmptyFields()) emptyFields.show();
+    else {
+      confirm.showAndWait();
+      if (confirm.getResult() == ButtonType.OK) {
+        pushSecurityRequestToDB();
+      }
+    }
+  }
+
+  private void pushSecurityRequestToDB() throws SQLException {
+    ArrayList<String> mdFields = new ArrayList<String>();
+    mdFields.add(threatChoice.getValue().toString());
+    System.out.println(threatChoice.getValue().toString());
   }
 
   public void cancelButton(ActionEvent event) {
-    roomNumber.clear();
+    clearFields();
+  }
+
+  private void clearFields() {
+    threatLevels.clear();
+    // locationBox.clear(); // TODO: locationBox needs a clear function
+  }
+
+  private boolean checkEmptyFields() {
+    return threatChoice.getSelectionModel().isEmpty() || locationBox.getSelectionModel().isEmpty();
+  }
+
+  private ArrayList<String> getLocations() {
+    ArrayList<String> locations = new ArrayList<>();
+    ArrayList<Location> locationsRaw = null;
+    try {
+      locationsRaw = LocationManager.getLocationManager().getAllLocations();
+    } catch (SQLException e) {
+      System.out.println("Failed to unearth locations from database");
+      e.printStackTrace();
+    }
+    for (Location l : locationsRaw) {
+      locations.add(l.getLongName());
+    }
+    return locations;
   }
 }
