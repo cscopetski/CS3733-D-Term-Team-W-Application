a4e6aea831dc9c413444f2094ec5673fa8748931
charkw <cckneisslwilliam@wpi.edu>
2022-04-18 00:46:45 -0400

    More employees

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wMid/Main.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wMid/Main.java
@@ -2,9 +2,12 @@ package edu.wpi.cs3733.d22.teamW.wMid;
 
 import edu.wpi.cs3733.d22.teamW.wDB.*;
 import edu.wpi.cs3733.d22.teamW.wDB.DAO.DBController;
+import edu.wpi.cs3733.d22.teamW.wDB.Managers.EmployeeManager;
 import edu.wpi.cs3733.d22.teamW.wDB.Managers.MedEquipManager;
+import edu.wpi.cs3733.d22.teamW.wDB.entity.Employee;
 import edu.wpi.cs3733.d22.teamW.wDB.entity.MedEquip;
 import edu.wpi.cs3733.d22.teamW.wDB.enums.DBConnectionMode;
+import edu.wpi.cs3733.d22.teamW.wDB.enums.EmployeeType;
 import edu.wpi.cs3733.d22.teamW.wDB.enums.MedEquipType;
 import java.util.ArrayList;
 
@@ -62,6 +65,105 @@ public class Main {
     for (MedEquip m : medEquipArrayList) {
       System.out.println(m.toCSVString());
     }
+
+    EmployeeManager em = EmployeeManager.getEmployeeManager();
+    Employee newEmployee7 =
+        new Employee(
+            7,
+            "Ethan",
+            "Pollack",
+            EmployeeType.Staff.getString(),
+            "ethan@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "ethan31",
+            "lead");
+    Employee newEmployee8 =
+        new Employee(
+            8,
+            "Jason",
+            "Odell",
+            EmployeeType.Staff.getString(),
+            "jason@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "uginghostdragon",
+            "assistant");
+    Employee newEmployee9 =
+        new Employee(
+            9,
+            "Edward",
+            "Enyedy",
+            EmployeeType.Staff.getString(),
+            "edward@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "Tarkus",
+            "wumboscrum");
+    Employee newEmployee10 =
+        new Employee(
+            10,
+            "Phil",
+            "Bui",
+            EmployeeType.Staff.getString(),
+            "phil@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "philbui",
+            "buiphil");
+    Employee newEmployee11 =
+        new Employee(
+            11,
+            "Jack",
+            "McEvoy",
+            EmployeeType.Staff.getString(),
+            "jack@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "LinkdAether",
+            "hi_jack");
+    Employee newEmployee12 =
+        new Employee(
+            12,
+            "Leona",
+            "Nguyen",
+            EmployeeType.Staff.getString(),
+            "leona@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "JustLeona",
+            "0822");
+    Employee newEmployee13 =
+        new Employee(
+            13,
+            "Dylan",
+            "Olmsted",
+            EmployeeType.Staff.getString(),
+            "dylan@hospital.com",
+            "(123) 456-7890",
+            "75 Francis St Boston MA 02115",
+            "SAFARIOLMY",
+            "8499");
+    Employee pitbull =
+        new Employee(
+            14,
+            "Armando",
+            "Perez",
+            EmployeeType.LanguageInterpreter.getString(),
+            "mrworldwide@gmail.com",
+            "(305) 305-1234",
+            "The Entire Earth",
+            "MrWorldWide",
+            "timber");
+    em.addEmployee(newEmployee7);
+    em.addEmployee(newEmployee8);
+    em.addEmployee(newEmployee9);
+    em.addEmployee(newEmployee10);
+    em.addEmployee(newEmployee11);
+    em.addEmployee(newEmployee12);
+    em.addEmployee(newEmployee13);
+    em.addEmployee(pitbull);
+    em.exportEmpCSV(employeesFileName);
 
     App.launch(App.class, args);
   }
