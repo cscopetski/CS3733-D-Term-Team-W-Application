de55bea0b9a9da3a41991410510fcc64df27ecd3
NhiNguyencmt8 <61450663+NhiNguyencmt8@users.noreply.github.com>
2022-04-22 07:07:44 -0400

    dashboardPage

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/DashBoardController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/DashBoardController.java
@@ -1,3 +1,31 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
-public class DashBoardController {}
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
+public class DashBoardController {
+
+  public Rectangle floor5Click;
+  public Rectangle floor4Click;
+  public Rectangle floor3Click;
+  public Rectangle floor2Click;
+  public Rectangle floor1Click;
+  public Rectangle LL1Click;
+  public Rectangle LL2Click;
+
+  public void clickFloor5(MouseEvent mouseEvent) {
+    floor5Click.setStyle("-fx-fill: #189da6");
+  }
+
+  public void clickFloor4(MouseEvent mouseEvent) {}
+
+  public void clickFloor3(MouseEvent mouseEvent) {}
+
+  public void clickFloor2(MouseEvent mouseEvent) {}
+
+  public void clickFloor1(MouseEvent mouseEvent) {}
+
+  public void clickLL1(MouseEvent mouseEvent) {}
+
+  public void clickLL2(MouseEvent mouseEvent) {}
+}
