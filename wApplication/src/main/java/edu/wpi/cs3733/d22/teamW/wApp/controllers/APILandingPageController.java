a8b19a5d5305f6e0cf196aedce7c895535d41d91
cscopetski <76232197+cscopetski@users.noreply.github.com>
2022-04-23 18:31:02 -0400

    Initial LangInterp API integration

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/APILandingPageController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/APILandingPageController.java
@@ -2,10 +2,17 @@ package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
 import edu.wpi.cs3733.d22.teamW.Managers.PageManager;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import edu.wpi.cs3733.d22.teamW.Managers.WindowManager;
+import edu.wpi.cs3733.d22.teamW.wDB.RequestFactory;
+import edu.wpi.cs3733.d22.teamW.wDB.enums.RequestType;
+import edu.wpi.teamW.API;
+import edu.wpi.teamW.ServiceException;
+import edu.wpi.teamW.dB.LanguageRequest;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+import edu.wpi.teamW.API.*;
 
 public class APILandingPageController {
 
@@ -25,7 +32,38 @@ public class APILandingPageController {
     }
 
     public void launchLanguageInterpreterAPI() {
-        PageManager.getInstance().loadPage(PageManager.Pages.MedicalEquipmentSR);
+        Integer isEmergency = 0;
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION, "Is this request an emergency?");
+        a.getButtonTypes().clear();
+        a.getButtonTypes().add(ButtonType.YES);
+        a.getButtonTypes().add(ButtonType.NO);
+        a.showAndWait();
+
+        if (a.getResult() == ButtonType.YES) {
+            isEmergency = 1;
+        }
+        try {
+            API.run(0,0,500,500,"","FDEPT00101","");
+
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("YO");
+        for(LanguageRequest lr : API.getAllRequests()){
+            System.out.println(lr.toString());
+            ArrayList<String> fields = new ArrayList<>();
+            fields.add(lr.getLanguage());
+            fields.add(lr.getNodeID());
+            fields.add(lr.getEmployee().getEmployeeID().toString());
+            fields.add(isEmergency.toString());
+            try {
+                RequestFactory.getRequestFactory().getRequest(RequestType.LanguageRequest,fields,false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("End");
     }
 
 
