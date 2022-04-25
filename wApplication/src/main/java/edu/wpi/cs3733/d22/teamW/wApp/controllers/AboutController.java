00a628137640a50c5543471d712ce0eb69ab0756
Jason Odell <jasonodell2001@gmail.com>
2022-04-25 19:23:49 -0400

    Began map controller

--- wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/AboutController.java
+++ wApplication/src/main/java/edu/wpi/cs3733/d22/teamW/wApp/controllers/AboutController.java
@@ -1,3 +1,14 @@
 package edu.wpi.cs3733.d22.teamW.wApp.controllers;
 
-public class AboutController {}
+import edu.wpi.cs3733.d22.teamW.wApp.controllers.customControls.HospitalMap;
+import edu.wpi.cs3733.d22.teamW.wDB.Errors.NonExistingMedEquip;
+
+import java.sql.SQLException;
+
+public class AboutController {
+    private HospitalMap map = new HospitalMap();
+
+    public AboutController() throws NonExistingMedEquip, SQLException {
+        map.setPrefSize(500, 800);
+    }
+}
